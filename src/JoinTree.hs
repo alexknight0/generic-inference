522d6067ce2ac40bd1e2dc8610df55e90af7c944
PoseidonOcto <octopus0246@gmail.com>
2025-07-14 08:35:12 +1000

    Added documentation.

--- src/JoinTree.hs
+++ src/JoinTree.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module JoinTree
@@ -13,13 +11,9 @@ import           Algebra.Graph
 import           Data.List        (union)
 import           Data.Set         (fromList, toList)
 import qualified Data.Set         (empty, union)
-import           Debug.Trace
 
 import           ValuationAlgebra
 
--- Note: while the 'Eq', 'Ord', and 'Show' instances can easily be generically written using
--- the exposed parts of the 'Node' typeclass, a typeclass cannot implement another typeclass.
--- Hence, this implementation must be deferred to the implementing type constructors.
 class Node n where
     collect         :: (Valuation v) => n v a b -> n v a b
     getValuation    :: (Valuation v) => n v a b -> v a b
@@ -27,50 +21,44 @@ class Node n where
     create          :: (Valuation v) => Integer -> Domain a -> v a b -> n v a b
     nodeId          ::                  n v a b -> Integer
 
--- could rename Integer to like NodeId
-
 setDifference :: (Eq a) => [a] -> [a] -> [a]
 setDifference xs ys = filter (\x -> not $ x `elem` ys) xs
 
-domainIntersects :: (Eq a) => [a] -> [a] -> Bool
-domainIntersects xs ys = or [x == y | x <- xs, y <- ys]
+{- | The core join tree construction algorithm that all required join trees are built from.
+This is precisely the join tree construction algorithm described on page 22 of
+"A Generic Architecture for Local Computation" by Marc Pouly.
 
--- Can replace 'nextNodeId' with a function that increments itself like for blockus.
---      A: Before we do this we should probably check it works, and write some tests.
--- The core join tree construction algorithm that all required join trees are built from.
--- This is precisely the join tree construction algorithm described on page 22 of
--- "A Generic Architecture for Local Computation" by Marc Pouly.
---
--- Creates a join tree where every valuation in the original list is assigned to a
--- leaf node. An effectively random node is assigned the root node. The terminology
--- 'leaf' node and 'root' node may be confusing in this context of directed trees:
---
---                                    5
---                           domain {F, L, D, B}
---
---                              ▲    ▲     ▲
---                              │    │     │
---                ┌─────────────┘    │     └──────┐
---                │                ┌─┘            │
---                │                │              │
---                │                │              │
---                │                │              │
---                   0            1               2
---          domain {F}    domain {L, F}    domain {D, F, B}
---
--- In this diagram, nodes 0, 1 and 2 are leaf nodes, and node 5 is the root node.
--- To make this even more confusing, nodes 0, 1 and 2 are all considered parents of
--- node 5 (and, accordingly, nodes 0, 1 and 5 consider node 5 their child).
---
--- Guarantees a numbering such that nodeId x < nodeId y for any node y that can be
--- reached from x. The node with the highest id is the root node. Some integers may
--- be skipped in the assignment of ids to nodes. All nodes either represent valuations,
--- or are either projection or union nodes. No node was added to represent an 'empty'
--- domain root.
---
--- The query domains are used only to ensure the join tree has a node that answers
--- each query (by creating empty nodes for each the query domain). The node ids
--- of the input valuations 'vs' in the final join tree are [0 .. (length vs)] respectively.
+Creates a join tree where every valuation in the original list is assigned to a
+leaf node. An effectively random node is assigned the root node. The terminology
+'leaf' node and 'root' node may be confusing in this context of directed trees:
+
+                                   5
+                          domain {F, L, D, B}
+
+                             ▲    ▲     ▲
+                             │    │     │
+               ┌─────────────┘    │     └──────┐
+               │                ┌─┘            │
+               │                │              │
+               │                │              │
+               │                │              │
+                  0            1               2
+         domain {F}    domain {L, F}    domain {D, F, B}
+
+In this diagram, nodes 0, 1 and 2 are leaf nodes, and node 5 is the root node.
+To make this even more confusing, nodes 0, 1 and 2 are all considered parents of
+node 5 (and, accordingly, nodes 0, 1 and 5 consider node 5 their child).
+
+Guarantees a numbering such that nodeId x < nodeId y for any node y that can be
+reached from x. The node with the highest id is the root node. Some integers may
+be skipped in the assignment of ids to nodes. All nodes either represent valuations,
+or are either projection or union nodes. No node was added to represent an 'empty'
+domain root.
+
+The query domains are used only to ensure the join tree has a node that answers
+each query (by creating empty nodes for each the query domain). The node ids
+of the input valuations 'vs' in the final join tree are [0 .. (length vs)] respectively.
+-}
 baseJoinTree :: forall n v a b. (Show a, Show b, Node n, Valuation v, Ord a, Ord b, Eq (n v a b))
     => [v a b]
     -> [Domain a]
@@ -87,6 +75,13 @@ baseJoinTree vs queries = edges $ baseJoinTree' nextNodeId r d
         nextNodeId :: Integer
         nextNodeId = fromIntegral $ length r
 
+{- | For an explanation of the overall algorithm see 'baseJoinTree'
+
+Where convenient, variables have been named as they appear in the pseudocode described on page 22 of
+"A Generic Architecture for Local Computation" by Marc Pouly. Not found in this pseudocode, 'nextNodeId'
+is a parameter such that there currently exists no nodes with id > 'nextNodeId' in the tree. As we may
+create up to 2 nodes on each iteration, we make the recursive call with 'nextNodeId + 2'
+-}
 baseJoinTree' :: forall n v a b. (Node n, Valuation v, Eq (n v a b), Ord a)
     => Integer
     -> [n v a b]
@@ -121,4 +116,4 @@ baseJoinTree' nextNodeId r (x : d')
             where
                 nPDomain = (fromList $ setDifference (toList domainOfPhiX) [x])
 
-
+data LowestCost
