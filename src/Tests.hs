82d0564f717c3c54a3df6fbbc4877f5cf3b34db9
PoseidonOcto <octopus0246@gmail.com>
2025-07-15 13:24:33 +1000

    Moved tests entry point.

---
+++ src/Tests.hs
@@ -0,0 +1,37 @@
+{- | Tests for the localcomputation library.
+
+Placed inside the library as Haskell Language Server can't dynamically reload the library when editing another component.
+No functions in the library should import any functionality from 'Tests' modules.
+-}
+module Tests
+    ( allTests
+    )
+where
+
+import qualified Tests.BayesianTests     as B
+import qualified Tests.FourierTests      as F
+import qualified Tests.MatrixTests       as M
+import qualified Tests.ShortestPathTests as S
+import qualified Tests.UtilsTests        as U
+
+{- | Hedgehog console output uses display regions rather than regular stdout logging, so may overwrite your stdout messages.
+
+To ensure consistent logging while displaying stdout, follow the following template:
+
+    import System.IO.Silently (capture)
+
+    ...
+
+    prop_example = ... $ do
+    ...
+    (msg, x) <- liftIO $ capture $ ...
+    annotate msg
+    ...
+
+Source: https://github.com/hedgehogqa/haskell-hedgehog/issues/236
+
+However, this may not work if your code spawns another process that does the logging. In this case, consider redirecting the
+hedgehog output to a file, so only the logging output remains. This can be done with `stack test > /dev/null`.
+-}
+allTests :: IO ()
+allTests = B.tests >> F.tests >> M.tests >> S.tests >> U.tests >> pure ()
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,30 +1,7 @@
-import qualified Tests.FourierTests       as F
-import qualified Tests.MatrixTests        as M
-import qualified Tests.ShortestPathTests  as S
-import qualified Tests.BayesianTests as B
-import qualified Tests.UtilsTests         as U
+import           Tests
 
-{- | Hedgehog console output uses display regions rather than regular stdout logging, so may overwrite your stdout messages.
-
-To ensure consistent logging while displaying stdout, follow the following template:
-
-    import System.IO.Silently (capture)
-
-    ...
-
-    prop_example = ... $ do
-    ...
-    (msg, x) <- liftIO $ capture $ ...
-    annotate msg
-    ...
-
-Source: https://github.com/hedgehogqa/haskell-hedgehog/issues/236
-
-However, this may not work if your code spawns another process that does the logging. In this case, consider redirecting the
-hedgehog output to a file, so only the logging output remains. This can be done with `stack test > /dev/null`.
--}
 main :: IO ()
-main = B.tests >> F.tests >> M.tests >> S.tests >> U.tests >> pure ()
+main = allTests
 
 
 
