97d3d922548e0cd91c356f9d207ae6f036458415
PoseidonOcto <octopus0246@gmail.com>
2025-08-27 17:32:47 +1000

    Changed svg backend to rasterization backend

--- src/LocalComputation/Inference/JoinTree/Diagram.hs
+++ src/LocalComputation/Inference/JoinTree/Diagram.hs
@@ -6,14 +6,16 @@ module LocalComputation.Inference.JoinTree.Diagram (
     draw
 ) where
 
-import           Diagrams.Backend.SVG                (renderSVG)
-import           Diagrams.Backend.SVG.CmdLine
+import           Diagrams.Backend.Rasterific         (renderRasterific)
+import           Diagrams.Backend.Rasterific.CmdLine
 import           Diagrams.Prelude
 
 import qualified Algebra.Graph                       as G
 import           Control.Exception                   (assert)
 import qualified Data.List                           as L
+import qualified Data.List.Extra                     as L (splitOn)
 import           Data.Maybe                          (fromJust)
+import           Diagrams.Backend.Rasterific.Text    (texterific)
 import qualified LocalComputation.Inference.JoinTree as JT
 
 myCircle :: Diagram B
@@ -23,7 +25,7 @@ myCircle = circle 1
 -- to unite all graphs under one data structure to prevent confusion.
 
 draw :: FilePath -> G.Graph (JT.Node a) -> IO ()
-draw name g = renderSVG name (dims2D 700 700) (tree g)
+draw name g = renderRasterific name (dims2D 700 700) (tree g)
 
 -- | Assumes a tree like structure, and that the node with the highest `id` is the root.
 tree :: G.Graph (JT.Node a) -> Diagram B
@@ -31,10 +33,13 @@ tree g = assert rootHasNoOutgoingEdges $ tree' root g
     where
         root = L.maximumBy (\x y -> x.id `compare` y.id) $ G.vertexList g
 
-        tree' _ _ = circle 1 ||| text "foobar" ||| text "fooooooooo of barssssss" ||| text "foo"
+        tree' _ _ = paragraph exampleText2
 
         rootHasNoOutgoingEdges = length rootOutgoingEdges == 0
         rootOutgoingEdges = snd . fromJust . L.find (\(x, adjacents) -> x.id == root.id) . G.adjacencyList $ g
+
+paragraph :: String -> Diagram B
+paragraph s = foldr ((===) . texterific) mempty $ L.splitOn "\n" s
 
 -- | Assumes the given node is in the tree.
 tree' :: JT.Node a -> G.Graph (JT.Node a) -> Diagram B
@@ -46,3 +51,17 @@ tree' node g = vsep 3 [root, parents] # applyAll (map (\parent -> connectOutside
         incoming = snd . fromJust . L.find (\(x, adjacents) -> x.id == node.id) . G.adjacencyList . G.transpose $ g
 
         parents = centerX . hsep 1 . map alignT . map (\n -> tree' n g) $ incoming
+
+
+exampleText2 =
+ "var   A   B   Probability                                 \n\
+ \                                                          \n\
+ \ 0    0   0       0.7       P(var == 0 | A == 0 && B == 0)\n\
+ \                                                          \n\
+ \ 0    0   1       0.4       P(var == 0 | A == 0 && B == 1)\n\
+ \                                                          \n\
+ \ 0    0   2       0.3       P(var == 0 | A == 0 && B == 2)\n\
+ \                                                          \n\
+ \ 0    1   0       0.6       P(var == 0 | A == 1 && B == 0)\n"
+
+exampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam risus augue, porta vitae fringilla quis, laoreet sed dui. \nVivamus bibendum pharetra lectus ac fringilla. Praesent vitae bibendum lectus. Etiam ut felis ultrices, tincidunt dolor sagittis, posuere lectus. Nullam rhoncus fringilla sem, quis congue metus egestas sit amet. Nulla eget nunc sit amet dui tincidunt faucibus sodales eget diam. Praesent odio lacus, molestie sit amet sem in, sollicitudin efficitur tellus. Donec efficitur est eu tellus condimentum fermentum. Donec eu lacinia mi. Pellentesque cursus lobortis tincidunt. Aenean consequat magna a lacinia ornare. Phasellus nulla est, feugiat sit amet turpis at, fermentum pretium dui. Aliquam quis varius eros, ut suscipit purus. Aliquam a tempus risus.\nAliquam hendrerit ipsum magna, quis porta justo scelerisque id. Aliquam erat volutpat. Integer eleifend lorem sollicitudin, porta ante non, sollicitudin lectus. Sed et venenatis arcu, consectetur ullamcorper massa. Phasellus quis mi id leo placerat malesuada. Nunc a pharetra leo. Donec ac diam blandit, auctor arcu nec, venenatis augue. Sed eget mattis risus. Quisque vitae laoreet lacus. Suspendisse potenti. Pellentesque commodo lorem libero, at hendrerit diam molestie lobortis. Cras nec tortor ut magna convallis venenatis rhoncus sed elit. Nam congue quam sit amet est placerat condimentum. Quisque orci ante, cursus non fermentum in, commodo a magna. Duis eleifend vestibulum orci id maximus. Duis fermentum congue dapibus.\nNulla nulla augue, tempus ut lectus nec, dictum faucibus elit. Aliquam ex neque, tincidunt sit amet interdum vitae, ultricies ut dui. Nulla gravida sagittis volutpat. Ut egestas facilisis sapien, vel blandit nisl maximus id. Integer dolor magna, euismod ac aliquam a, tincidunt vitae libero. Phasellus justo felis, cursus id vulputate sit amet, maximus at sem. Nulla nec vestibulum lorem, a scelerisque justo. Cras et ultricies leo, et luctus quam. Curabitur sollicitudin suscipit purus, vel semper turpis feugiat eu. Etiam vehicula et lorem quis porttitor.\nMaecenas semper libero id rutrum auctor. Mauris a mollis turpis, a pretium odio. Nullam elementum porta magna quis placerat. Donec fringilla diam nibh, vitae scelerisque orci hendrerit quis. In finibus consectetur felis. Sed accumsan pretium augue a ornare. Morbi vulputate, lectus vitae feugiat ornare, mauris augue pellentesque diam, at dapibus sapien urna vitae dolor. Suspendisse potenti. Pellentesque ut mi justo. Aliquam finibus ligula quis finibus cursus. Aenean egestas nibh nec finibus volutpat. Cras gravida quis tellus eu venenatis. Pellentesque quis ullamcorper lacus."
