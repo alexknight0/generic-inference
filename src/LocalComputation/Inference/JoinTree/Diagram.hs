ecdb8abc9979ce164deed9fb257f5f7cf0f3fbaa
PoseidonOcto <octopus0246@gmail.com>
2025-08-29 13:12:32 +1000

    Added documentation

--- src/LocalComputation/Inference/JoinTree/Diagram.hs
+++ src/LocalComputation/Inference/JoinTree/Diagram.hs
@@ -7,11 +7,9 @@ module LocalComputation.Inference.JoinTree.Diagram (
     draw
 ) where
 
--- TODO: Remove Data.Colour package from package.yaml
 import qualified Data.Colour                         as C
 import           Diagrams.Backend.SVG                (renderSVG)
 import           Diagrams.Backend.SVG.CmdLine
--- import qualified Diagrams.Color.XKCD                 as C
 import           Diagrams.Prelude
 import qualified Graphics.SVGFonts                   as SF
 
@@ -24,9 +22,15 @@ import qualified Graphics.SVGFonts.ReadFont          as SF
 import qualified LocalComputation.Inference.JoinTree as JT
 import qualified LocalComputation.ValuationAlgebra   as V
 
+data DiagramWithBorder a = DiagramWithBorder {
+    diagram     :: Diagram a,
+    borderWidth :: Double
+}
+
 -- TODO: A different data structure for the graph could be better here. Additionally, we might want
 -- to unite all graphs under one data structure to prevent confusion.
 
+-- Draws a tree from the given graph, outputting the drawing into a file of the given name. Has the same assumptions as `tree`.
 draw :: (V.Valuation v, Show (v a b), Ord a, Ord b, Show b, Show a)
     => FilePath -> G.Graph (JT.Node (v a b)) -> IO ()
 draw name g = do
@@ -37,21 +41,10 @@ draw name g = do
 
     renderSVG name (dims2D 1400 1400) (diagram # framePadding 0.05)
 
--- | Scales a diagram so that it is proportional to another diagram.
+-- | Produces a diagram of a tree out of a given graph.
 --
--- Might want to look into `requiredScaling`.
-scaleProportional :: Double -> Diagram B -> Diagram B -> Diagram B
-scaleProportional ratio reference x = x # scale result
-    where
-        newWidth  = ratio * width  reference
-        newHeight = ratio * height reference
-
-        requiredWidthScaling  = newWidth  / width  x
-        requiredHeightScaling = newHeight / height x
-
-        result = min requiredWidthScaling requiredHeightScaling
-
--- | Assumes a tree like structure, and that the node with the highest `id` is the root.
+-- Assumes the given graph has a tree like structure, as specified in the description of `baseJoinTree`,
+-- and that the node with the highest `id` is the root.
 tree :: (V.Valuation v, Show (v a b), Ord a, Ord b, Show b, Show a)
     => SF.PreparedFont Double -> G.Graph (JT.Node (v a b)) -> Diagram B
 tree chosenFont g = assert (length rootOutgoingEdges == 0) $
@@ -61,34 +54,26 @@ tree chosenFont g = assert (length rootOutgoingEdges == 0) $
 
         rootOutgoingEdges = snd . fromJust . L.find (\(x, _) -> x.id == root.id) . G.adjacencyList $ g
 
-data DiagramWithBorder a = DiagramWithBorder {
-    diagram     :: Diagram a,
-    borderWidth :: Double
-}
-
-framePadding :: Double -> Diagram B -> Diagram B
-framePadding ratio x = x # frame (ratio * min (width x) (height x))
-
-withBorder :: Diagram B -> DiagramWithBorder B
-withBorder x = DiagramWithBorder ((textWithPadding <> rectangle) # withEnvelope rectangle)
-                              borderWidth
+-- | Produces a diagram of a tree by following all **incoming** edges from a given node in a graph. Assumes the given node is in the tree.
+tree' :: (V.Valuation v, Show (v a b), Ord a, Ord b, Show b, Show a)
+    => SF.PreparedFont Double -> JT.Node (v a b) -> G.Graph (JT.Node (v a b)) -> Diagram B
+tree' chosenFont node g = vsep vgap [root.diagram, parents] # applyAll arrows
     where
-        paddingSize = min (width x) (height x) * 0.1
-        textWithPadding = centerXY $ strutY (paddingSize / 2) === (strutX paddingSize ||| x # centerXY ||| strutX paddingSize) === strutY paddingSize
+        -- root = text (show node.id) <> circle 1 # named node.id
+        root = treeNode chosenFont node
 
-        rectangle = rect (width textWithPadding) (height textWithPadding) # borderStyles
-        borderWidth = 0.2 * paddingSize
-        borderStyles = lwL (borderWidth / 2) # dashingL [borderWidth * 2, borderWidth * 3] 0 # opacity 0.8
+        -- Horribly inefficent, constantly recalculated
+        incoming = snd . fromJust . L.find (\(x, _) -> x.id == node.id) . G.adjacencyList . G.transpose $ g
+        parents = centerX . hsep hgap . map alignT . map (\n -> tree' chosenFont n g) $ incoming
 
+        arrows = map (\parent -> connectOutside' arrowOpts parent.id node.id # lwL root.borderWidth) incoming
+        arrowOpts = with & headLength .~ local (root.borderWidth * 8)
+                         & headGap    .~ local (root.borderWidth * 6)
+                         & tailGap    .~ local (root.borderWidth * 6)
+        vgap = 0.5  * height root.diagram
+        hgap = 0.25 * width root.diagram
 
-textWithNewlines :: SF.PreparedFont Double -> Colour Double -> String -> Diagram B
-textWithNewlines chosenFont colour s = body # centerXY
-    where
-        body = foldr ((===) . textWithEnvelope chosenFont colour) mempty $ L.splitOn "\n" s
-
-textWithEnvelope :: SF.PreparedFont Double -> Colour Double -> String -> Diagram B
-textWithEnvelope chosenFont colour s = SF.svgText (SF.TextOpts chosenFont SF.HADV False) s # SF.set_envelope # lw none # fc colour
-
+-- | Produces a diagram representing a node in a tree.
 treeNode :: (V.Valuation v, Show (v a b), Ord a, Ord b, Show b, Show a)
     => SF.PreparedFont Double -> JT.Node (v a b) -> DiagramWithBorder B
 treeNode chosenFont node = DiagramWithBorder (full # named node.id) contents.borderWidth
@@ -110,44 +95,7 @@ treeNode chosenFont node = DiagramWithBorder (full # named node.id) contents.bor
 
         seperationSpace = height domain / 4
 
--- | Variant of `bg` that allows alpha values.
---
--- __Warning__: Will center local origin of diagram.
-bgA :: AlphaColour Double -> Diagram B -> Diagram B
-bgA colour x = x # centerXY
-                    <> rect (width x) (height x) # centerXY # lw 0 # fcA colour
-
--- | Creates a background of a fixed size.
---
--- __Warning__: Will center local origin of diagram.
-fixedBgA :: Double -> Double -> AlphaColour Double -> Diagram B -> Diagram B
-fixedBgA w h colour x = x # centerXY
-                    <> rect w h # centerXY # lw 0 # fcA colour
-
-line :: Double -> Diagram B
-line w = arrowV' arrowOpts (r2 (w, 0)) # lw ultraThin # centerXY
-    where
-        arrowOpts = with & arrowHead .~ noHead
-
--- | Assumes the given node is in the tree.
-tree' :: (V.Valuation v, Show (v a b), Ord a, Ord b, Show b, Show a)
-    => SF.PreparedFont Double -> JT.Node (v a b) -> G.Graph (JT.Node (v a b)) -> Diagram B
-tree' chosenFont node g = vsep vgap [root.diagram, parents] # applyAll arrows
-    where
-        -- root = text (show node.id) <> circle 1 # named node.id
-        root = treeNode chosenFont node
-
-        -- Horribly inefficent, constantly recalculated
-        incoming = snd . fromJust . L.find (\(x, _) -> x.id == node.id) . G.adjacencyList . G.transpose $ g
-        parents = centerX . hsep hgap . map alignT . map (\n -> tree' chosenFont n g) $ incoming
-
-        arrows = map (\parent -> connectOutside' arrowOpts parent.id node.id # lwL root.borderWidth) incoming
-        arrowOpts = with & headLength .~ local (root.borderWidth * 8)
-                         & headGap    .~ local (root.borderWidth * 6)
-                         & tailGap    .~ local (root.borderWidth * 6)
-        vgap = 0.5  * height root.diagram
-        hgap = 0.25 * width root.diagram
-
+-- | Returns the colour for a given node type.
 nodeColour :: JT.NodeType -> AlphaColour Double
 nodeColour t = case t of
                   JT.Valuation  -> darkblue              `C.withOpacity` 0.15
@@ -155,6 +103,7 @@ nodeColour t = case t of
                   JT.Union      -> orange                `C.withOpacity` 0.15
                   JT.Projection -> darken 0.5 darkorange `C.withOpacity` 0.15
 
+-- | A legend identifying the colours of all possible node types.
 legend :: SF.PreparedFont Double -> Diagram B
 legend chosenFont = full <> border
     where
@@ -168,8 +117,74 @@ legend chosenFont = full <> border
         nodeTypes = [minBound .. maxBound]
 
         entriesText = map     (\t -> textWithNewlines chosenFont black (show t ++ " Node")) nodeTypes
-        entries     = zipWith (\t e -> fixedBgA w h (nodeColour t) e)            nodeTypes entriesText
+        entries     = zipWith (\t e -> fixedBgA w h (nodeColour t) e)                       nodeTypes entriesText
 
         w = 1.5 * maximum (map width  (headerText : entriesText))
         h = 1.5 * maximum (map height (headerText : entriesText))
 
+--------------------------------------------------------------------------------
+-- Utilities
+--------------------------------------------------------------------------------
+
+-- | Adds a border to the given element. Used with `treeNode`.
+withBorder :: Diagram B -> DiagramWithBorder B
+withBorder x = DiagramWithBorder ((textWithPadding <> rectangle) # withEnvelope rectangle)
+                              borderWidth
+    where
+        paddingSize = min (width x) (height x) * 0.1
+        textWithPadding = centerXY $ strutY (paddingSize / 2) === (strutX paddingSize ||| x # centerXY ||| strutX paddingSize) === strutY paddingSize
+
+        rectangle = rect (width textWithPadding) (height textWithPadding) # borderStyles
+        borderWidth = 0.2 * paddingSize
+        borderStyles = lwL (borderWidth / 2) # dashingL [borderWidth * 2, borderWidth * 3] 0 # opacity 0.8
+
+-- | A line of width 'w'.
+line :: Double -> Diagram B
+line w = arrowV' arrowOpts (r2 (w, 0)) # lw ultraThin # centerXY
+    where
+        arrowOpts = with & arrowHead .~ noHead
+
+-- | Variant of `textWithEnvelope` that handles newlines by putting the text on a new line.
+textWithNewlines :: SF.PreparedFont Double -> Colour Double -> String -> Diagram B
+textWithNewlines chosenFont colour s = body # centerXY
+    where
+        body = foldr ((===) . textWithEnvelope chosenFont colour) mempty $ L.splitOn "\n" s
+
+-- | Text that has an envelope representing its size.
+textWithEnvelope :: SF.PreparedFont Double -> Colour Double -> String -> Diagram B
+textWithEnvelope chosenFont colour s = SF.svgText (SF.TextOpts chosenFont SF.HADV False) s # SF.set_envelope # lw none # fc colour
+
+
+-- | Alternative to `pad` that is not affected by whether the diagram is centred.
+-- Pads all sides by the same amount, proportional to the ratio multiplied by the
+-- minimum of the weight and height.
+framePadding :: Double -> Diagram B -> Diagram B
+framePadding ratio x = x # frame (ratio * min (width x) (height x))
+
+-- | Scales a diagram so that it is proportional to another diagram.
+--
+-- Might want to look into `requiredScaling`.
+scaleProportional :: Double -> Diagram B -> Diagram B -> Diagram B
+scaleProportional ratio reference x = x # scale result
+    where
+        newWidth  = ratio * width  reference
+        newHeight = ratio * height reference
+
+        requiredWidthScaling  = newWidth  / width  x
+        requiredHeightScaling = newHeight / height x
+
+        result = min requiredWidthScaling requiredHeightScaling
+
+-- | Variant of `bg` that allows alpha values.
+--
+-- __Warning__: Will center local origin of diagram, and may not behave like `bg` does.
+bgA :: AlphaColour Double -> Diagram B -> Diagram B
+bgA colour x = x # centerXY
+                    <> rect (width x) (height x) # centerXY # lw 0 # fcA colour
+
+-- | Creates a background of a fixed size.
+--
+-- __Warning__: Will center local origin of diagram, and may not behave like `bg` does.
+fixedBgA :: Double -> Double -> AlphaColour Double -> Diagram B -> Diagram B
+fixedBgA w h colour x = x # centerXY
+                    <> rect w h # centerXY # lw 0 # fcA colour
